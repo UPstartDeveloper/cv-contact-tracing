1c04679dffb34c51806d75aa47a2ab147b60ed38
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-07 12:51:23 -0700

    Added is_touching collision method

--- touchtrail/scene.py
+++ touchtrail/scene.py
@@ -31,7 +31,23 @@ class Object():
 
     def is_touching(self, other):
         """Determines if two objects have come into contact by calculating
-        overlap between two bouding boxes.
+        overlap between two bouding boxes. Assumes coordinate system starts
+        at top left.
+
+        Collision Examples:
+                                                     __________________
+                               ___________          |                  |
+         _________            |           |         |    _____   G     |
+        |         |           |     C     |         |   |     |        |
+        |    A    |___________|__         |         |   |  F  |        |
+        |_________|              |________|         |   |_____|        |
+                |                |                  |__________________|
+                |       B        |
+            ____|            ____|____       ___________
+           |    |___________|         |     |          _|________
+           |     |          |    D    |     |    H    |    I     |
+           |  E  |          |_________|     |         |__________|
+           |_____|                          |___________|
 
         Args:
             other (Object): The Object to compare.
@@ -39,8 +55,14 @@ class Object():
         Returns:
             True if this object overlaps with other, else False.
         """
-        raise NotImplemented
+        right = self.left + self.width
+        bottom = self.top + self.height
+        other_right = other.left + other.width
+        other_bottom = other.top + other.height
 
+        return (right < other.left or self.left > other_right or
+                bottom < other.top or self.top > other_bottom)
+        
 class Person(Object):
     """Maintains properties for a person object."""
 
