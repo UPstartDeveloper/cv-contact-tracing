337ff2ffc159f7484d5161bfc4126defc1926ba4
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-07 11:05:18 -0700

    New base classes with docstrings for managing a scene

---
+++ touchtrail/scene.py
@@ -0,0 +1,28 @@
+"""This module manages all the components of a computer vision scene.
+
+Classes are used to manage all the people and things in a scene. Every scene has
+a place as well, which is managed by the Place class.
+
+    Typical usage example:
+
+"""
+
+
+class Person():
+    """Maintains properties for a person object."""
+    pass
+
+
+class Place():
+    """Maintains properties about the location of a scene."""
+    pass
+
+
+class Thing():
+    """Maintains properties for things that are objects."""
+    pass
+
+class Scene():
+    """Maintains a coherent scene narrative consisting of locations, paths,
+    collisions, and Person-Thing contacts."""
+    pass
